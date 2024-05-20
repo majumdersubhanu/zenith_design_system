ceba2c3d8aede6f7dbe3fe7d1007623645a8e0da
subhanu-majumder <reach.subhanu@gmail.com>
2024-05-20 13:02:17 +0530

    app text styles configured

---
+++ lib/typography/typography.dart
@@ -0,0 +1,130 @@
+// lib/typography/app_text_style.dart
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTextStyles {
+  // Heading Styles
+  static final TextStyle headingXl = GoogleFonts.ibmPlexSans(
+    fontSize: 60,
+    height: 78 / 60,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle headingXlBold = GoogleFonts.ibmPlexSans(
+    fontSize: 60,
+    height: 78 / 60,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle headingLg = GoogleFonts.ibmPlexSans(
+    fontSize: 48,
+    height: 64 / 48,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle headingLgBold = GoogleFonts.ibmPlexSans(
+    fontSize: 48,
+    height: 64 / 48,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle headingMd = GoogleFonts.ibmPlexSans(
+    fontSize: 34,
+    height: 48 / 34,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle headingMdBold = GoogleFonts.ibmPlexSans(
+    fontSize: 34,
+    height: 48 / 34,
+    fontWeight: FontWeight.w700,
+  );
+
+  // Title Styles
+  static final TextStyle titleXl = GoogleFonts.ibmPlexSans(
+    fontSize: 24,
+    height: 34 / 24,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle titleXlBold = GoogleFonts.ibmPlexSans(
+    fontSize: 24,
+    height: 34 / 24,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle titleLg = GoogleFonts.ibmPlexSans(
+    fontSize: 20,
+    height: 30 / 20,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle titleLgBold = GoogleFonts.ibmPlexSans(
+    fontSize: 20,
+    height: 30 / 20,
+    fontWeight: FontWeight.w700,
+  );
+
+  // Body Styles
+  static final TextStyle bodyXl = GoogleFonts.ibmPlexSans(
+    fontSize: 18,
+    height: 28 / 18,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle bodyXlBold = GoogleFonts.ibmPlexSans(
+    fontSize: 18,
+    height: 28 / 18,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bodyLg = GoogleFonts.ibmPlexSans(
+    fontSize: 16,
+    height: 24 / 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle bodyLgBold = GoogleFonts.ibmPlexSans(
+    fontSize: 16,
+    height: 24 / 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bodyMd = GoogleFonts.ibmPlexSans(
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle bodyMdBold = GoogleFonts.ibmPlexSans(
+    fontSize: 14,
+    height: 20 / 14,
+    fontWeight: FontWeight.w700,
+  );
+
+  static final TextStyle bodySm = GoogleFonts.ibmPlexSans(
+    fontSize: 12,
+    height: 18 / 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle bodySmBold = GoogleFonts.ibmPlexSans(
+    fontSize: 12,
+    height: 18 / 12,
+    fontWeight: FontWeight.w700,
+  );
+
+  // Caption Style
+  static final TextStyle caption = GoogleFonts.ibmPlexSans(
+    fontSize: 10,
+    height: 14 / 10,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle captionBold = GoogleFonts.ibmPlexSans(
+    fontSize: 10,
+    height: 14 / 10,
+    fontWeight: FontWeight.w700,
+  );
+}
