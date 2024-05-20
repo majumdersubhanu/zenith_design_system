2bfbb5ef55697442c44cb06c5fbda20f192f04f5
subhanu-majumder <reach.subhanu@gmail.com>
2024-05-20 12:52:51 +0530

    app colors configured

---
+++ lib/colors/app_colors.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Surface colors
+  final surface50 = const Color(0xFFFFFFFF);
+  final surface100 = const Color(0xFFF7F9FD);
+  final surface200 = const Color(0xFFECEDEE);
+  final surface300 = const Color(0xFFCBCFD2);
+  final surface400 = const Color(0xFFA7AEB4);
+  final surface500 = const Color(0xFF8E979F);
+  final surface600 = const Color(0xFF6B767F);
+  final surface700 = const Color(0xFF4F575E);
+  final surface800 = const Color(0xFF33383D);
+  final surface900 = const Color(0xFF202327);
+
+  // Primary colors
+  final primary50 = const Color(0xFFF8F5FF);
+  final primary100 = const Color(0xFFB89CFC);
+  final primary200 = const Color(0xFF7D49F8);
+  final primary300 = const Color(0xFF6528F7);
+  final primary400 = const Color(0xFF501BCE);
+
+  // Secondary colors
+  final secondary50 = const Color(0xFFE8F1FC);
+  final secondary100 = const Color(0xFF77A9EE);
+  final secondary200 = const Color(0xFF71B6DDF);
+  final secondary300 = const Color(0xFF1450A3);
+  final secondary400 = const Color(0xFF114388);
+
+  // Danger colors
+  final danger50 = const Color(0xFFFBEAEA);
+  final danger100 = const Color(0xFFE16B6B);
+  final danger200 = const Color(0xFFC92929);
+  final danger300 = const Color(0xFFA92323);
+  final danger400 = const Color(0xFF6E1717);
+
+  // Success colors
+  final success50 = const Color(0xFFF6FEFB);
+  final success100 = const Color(0xFF1BC590);
+  final success200 = const Color(0xFF12825F);
+  final success300 = const Color(0xFF0E674B);
+  final success400 = const Color(0xFF0A4834);
+
+  // Warning colors
+  final warning50 = const Color(0xFFFFFDFA);
+  final warning100 = const Color(0xFFF9DBB5);
+  final warning200 = const Color(0xFFF2AB53);
+  final warning300 = const Color(0xFFEE9322);
+  final warning400 = const Color(0xFFA6620D);
+
+  // Opacity colors
+  final opacityPrimary = const Color(0xFF7D49F8).withOpacity(0.15);
+  final opacitySecondary = const Color(0xFF1C6FE3).withOpacity(0.15);
+  final opacityDanger = const Color(0xFFC92929).withOpacity(0.15);
+  final opacitySuccess = const Color(0xFF12825F).withOpacity(0.15);
+  final opacityWarning = const Color(0xFFF2AB53).withOpacity(0.15);
+  final opacityModal = const Color(0xFF121212).withOpacity(0.92);
+
+  // Dark colors
+  final dark50 = const Color(0xFF121212);
+  final dark100 = const Color(0xFF161616);
+  final dark200 = const Color(0xFF1A1A1A);
+  final dark300 = const Color(0xFF1C1C1C);
+  final dark400 = const Color(0xFF1E1E1E);
+}
