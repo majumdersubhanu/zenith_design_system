bfd222068328da50d6368d1a66dd6757cfe68795
subhanu-majumder <reach.subhanu@gmail.com>
2024-05-20 13:25:27 +0530

    app spacing configured

---
+++ lib/spacing/app_spacing.dart
@@ -0,0 +1,21 @@
+class AppSpacing {
+  static const double space0 = 0.0;
+  static const double space1 = 4.0;
+  static const double space2 = 8.0;
+  static const double space3 = 12.0;
+  static const double space4 = 16.0;
+  static const double space5 = 20.0;
+  static const double space6 = 24.0;
+  static const double space7 = 28.0;
+  static const double space8 = 32.0;
+  static const double space9 = 36.0;
+  static const double space10 = 40.0;
+  static const double space11 = 44.0;
+  static const double space12 = 48.0;
+  static const double space13 = 56.0;
+  static const double space14 = 60.0;
+  static const double space15 = 64.0;
+  static const double space16 = 68.0;
+  static const double space17 = 80.0;
+  static const double space18 = 96.0;
+}
